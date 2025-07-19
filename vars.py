bf62c479803c6cea21b49fa7574fa57682f69a50
makwanaram <makwanaram007@gmail.com>
2025-07-19 17:40:25 +0530

    fiesr

---
+++ vars.py
@@ -0,0 +1,6 @@
+git in i# Don't Remove Credit Telegram - @Engineers_Babu
+from os import environ
+
+API_ID = int(environ.get("API_ID", "22727464"))
+API_HASH = environ.get("API_HASH", "f0e595a263c89aa17f6571b8af296ced")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7110231022:AAHzjuE3MR7FdUvdFnaEHKurmQ9526aTIiM")
