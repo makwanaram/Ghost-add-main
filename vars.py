f86c101b87fa601bddeb4db42bbf151f299edfa7
makwanaram <makwanaram007@gmail.com>
2025-07-21 21:02:15 +0530

    saini bot change

--- vars.py
+++ vars.py
@@ -3,9 +3,9 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22727464"))
-API_HASH = environ.get("API_HASH", "f0e595a263c89aa17f6571b8af296ced")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7110231022:AAHzjuE3MR7FdUvdFnaEHKurmQ9526aTIiM")
+API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
+API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = int(environ.get("OWNER", "887699812"))
 CREDIT = environ.get("CREDIT", "JACK BOT")
